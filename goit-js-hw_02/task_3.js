1f34593ba9d2ae10f3a4f45b7e2aadd53cc35a3b
Alinchyk <104371610+Alinchyk@users.noreply.github.com>
2022-07-26 18:48:29 +0200

    Add files via upload

---
+++ goit-js-hw_02/task_3.js
@@ -0,0 +1,19 @@
+// Напиши функцію findLongestWord(string), яка приймає параметром довільний рядок (в рядку будуть тільки слова і прогалини) і повертає найдовше слово в цьому рядку.
+
+const findLongestWord = function (string) {
+  const stringSplit = string.split(" ");
+  let longestWord = "";
+
+  for (const word of stringSplit) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  return longestWord;
+};
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
+
+console.log(findLongestWord("Google do a roll")); // 'Google'
+
+console.log(findLongestWord("May the force be with you")); // 'force'
