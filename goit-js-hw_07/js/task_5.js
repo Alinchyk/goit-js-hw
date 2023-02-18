e66915e449441b564c6634af981eb95ca2b08b84
Alinchyk <alina.mykhalchuk09@gmail.com>
2023-02-18 21:16:20 +0100

    add task changes

--- goit-js-hw_07/js/task_5.js
+++ goit-js-hw_07/js/task_5.js
@@ -1,15 +1,15 @@
-// // Напиши скрипт який, при наборі тексту в інпут input#name-input (подія input), підставляє його поточне значення в span#name-output. якщо інпут порожній, в спані повинен відображатися рядок 'незнайомець'.
+// Напиши скрипт який, при наборі тексту в інпут input#name-input (подія input), підставляє його поточне значення в span#name-output. якщо інпут порожній, в спані повинен відображатися рядок 'незнайомець'.
 
-// // {
-// // <input type="text" placeholder="Ваше ім'я?" id="name-input" />
-// // <h1>Привіт, <span id="name-output">незнайомець</span>!</h1> */
-// // }
+// {
+// <input type="text" placeholder="Ваше ім'я?" id="name-input" />
+// <h1>Привіт, <span id="name-output">незнайомець</span>!</h1> */
+// }
 
 const input = document.querySelector("#name-input");
 
 input.addEventListener("keyup", function (e) {
   const output = document.querySelector("#name-output");
-  if (input.value === "") {
+  if (input.value.trim() === "") {
     output.textContent = "незнайомець";
   } else {
     output.textContent = e.target.value;
