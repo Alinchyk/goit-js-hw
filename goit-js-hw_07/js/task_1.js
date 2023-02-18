47b2851e11cf473b02da1cfe4316dd4ad138582a
Alinchyk <alina.mykhalchuk09@gmail.com>
2023-02-18 21:15:53 +0100

    add task changes

--- goit-js-hw_07/js/task_1.js
+++ goit-js-hw_07/js/task_1.js
@@ -45,12 +45,12 @@
 // Кількість елементів: 4
 
 const searchLi = document.querySelectorAll(".item");
+
+// підрахунок і виведення категорій
 console.log(`У списку ${searchLi.length} категорії`);
 
+// підрахунок і виведення інформації категорій
 searchLi.forEach(searchElem => {
-  console.log(
-    `Категорія: ${searchElem.querySelector("h2").textContent} Кількість елементів: ${
-      searchElem.querySelectorAll(".item li").length
-    }`,
-  );
+  console.log(`Категорія: ${searchElem.querySelector("h2").textContent}`);
+  console.log(`Кількість елементів: ${searchElem.querySelectorAll(".item li").length}`);
 });
